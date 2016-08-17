1e5ca76dc0b06086d24b01b08d05e84053b00c5b
NCrashed <ncrashed@gmail.com>
2016-08-17 13:25:45 +0300

    Add second part of task

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Main where 
 
 import Data.Proxy 
@@ -11,20 +12,13 @@ import Data.Typeable
 import Data.Vinyl
 import GHC.TypeLits 
 import Servant.API 
+import Servant.Client
 import Text.Read 
 
-type family APIToFields' (acc :: [(Symbol, *)]) layout where 
-  APIToFields' acc (QueryParam s a :> b) = APIToFields' ( '(s, a) ': acc) b
-  APIToFields' acc (a :> b) = APIToFields' acc b
-  APIToFields' acc a = acc
-
-type family Reverse (acc :: [(Symbol, *)]) ls where 
-  Reverse acc '[] = acc
-  Reverse acc (a ': ls) = Reverse (a ': acc) ls 
-
--- wrapper for simplicity
-type family APIToFields layout where 
-  APIToFields layout = Reverse '[] (APIToFields' '[] layout)
+type family APIToFields layout :: [(Symbol, *)] where 
+  APIToFields (QueryParam s a :> b) = '(s, a) ': APIToFields b 
+  APIToFields (a :> b) = APIToFields b 
+  APIToFields a = '[]
 
 class ComParser api where 
   type ComParserRes api :: * 
@@ -50,12 +44,42 @@ instance ComParser (Verb (a :: StdMethod) b c d) where
   type ComParserRes (Verb a b c d) = FieldRec '[]
   parser _ _ = Just RNil 
 
+class HasClient api => ConvertAPI api input where
+  type FinalType api :: *
+
+  convertClient :: Proxy api -> input -> Client api -> FinalType api
+
+instance (
+    ConvertAPI b (FieldRec as)
+  , KnownSymbol s
+  , ToHttpApiData a
+  ) => ConvertAPI (QueryParam s a :> b) (FieldRec ( '(s, a) ': as)) where
+  type FinalType (QueryParam s a :> b) = FinalType b
+
+  convertClient _ (Field a :& as) cl = convertClient (Proxy :: Proxy b) as (cl $ Just a) 
+
+instance (
+    ReflectMethod a
+  , MimeUnrender ct d
+  ) => ConvertAPI (Verb (a :: StdMethod) b (ct ': cts) d) (FieldRec fields) where 
+  type FinalType (Verb a b (ct ': cts) d) = Client (Verb a b (ct ': cts) d)
+
+  convertClient _ _ cl = cl 
+
 type API = QueryParam "a" Int :> QueryParam "b" Double :> QueryParam "c" Bool :> Get '[JSON] ()
+
+convert :: ConvertAPI api input => Proxy api -> input -> FinalType api
+convert prx input = convertClient prx input $ client prx
 
 main :: IO ()
 main = do 
-  let apiParser = parser (Proxy :: Proxy API) 
+  let 
+    api = Proxy :: Proxy API
+    apiParser = parser api
   print $ apiParser ["1", "42.0", "True"]
   print $ apiParser ["1.0", "42.0", "True"]
   print $ apiParser ["1.0", "42.0"]
   print $ apiParser ["1", "42.0", "True", ""]
+
+  let Just input = apiParser ["1", "42.0", "True"]
+  print $ typeOf (convert api input)
